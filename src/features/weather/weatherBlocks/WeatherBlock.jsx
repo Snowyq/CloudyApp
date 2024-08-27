b4d4eb84a6f8d983ada1ef08b4ec94e67c391886
Jakub Pniewski <jakub.pniewski@icloud.com>
2024-08-27 20:09:25 +0200

    some fixes

--- src/features/weather/weatherBlocks/WeatherBlock.jsx
+++ src/features/weather/weatherBlocks/WeatherBlock.jsx
@@ -1,7 +1,7 @@
 function WeatherBlock({ children, onClick, className = '' }) {
   return (
     <div
-      className={`relative rounded-lg bg-neutral-700 px-1 py-0.5 text-neutral-300 transition-all ${className} transition-all hover:scale-[1.02]`}
+      className={`relative rounded-lg bg-neutral-700 px-1 py-0.5 text-neutral-300 transition-all ${className} transition-all`}
       onClick={onClick}
     >
       {children}
